239017bdee66cc33e2a6aa7126ba660860adc324
chathurangaj <chathuranga.jayanath.1999@gmail.com>
2024-09-16 00:18:52 +0530

    add read file line by line

--- utils.py
+++ utils.py
@@ -7,3 +7,17 @@ def read_txt_file(file_path):
         return "File not found. Please check the file path."
     except Exception as e:
         return f"An error occurred: {e}"
+
+def read_file_line_by_line_as_list(file_path):
+    try:
+        result = []
+        with open(file_path, 'r') as file:
+            for line in file:
+                stripped_line = line.strip()
+                if stripped_line != "":
+                    result.append(stripped_line)
+        return result
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
